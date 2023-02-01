3d176446a448fa8173ed1bff6d5a0b7860605afd
electriclance <b24.lance.libatique@pshs.edu.ph>
2023-02-01 15:37:49 +0800

    [UPD] General file structure
    added new folders

--- handistry-app/src/App.tsx
+++ handistry-app/src/App.tsx
@@ -1,5 +1,4 @@
 import React, {useEffect} from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -10,9 +9,8 @@ function App() {
     //Code here the main app component
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>srcasdasdads/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
