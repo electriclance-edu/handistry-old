85cb7619145daa74271100e83bf1053e34e70247
sullvits <77877131+sullvits@users.noreply.github.com>
2023-04-20 10:45:28 +0800

    [UPD] Remade stockroom screen's layout - each shelf has its own z-index to accomodate equipment on the shelves - changed class equipmentGenerator to eqptRow-gen to follow class naming   and also for shorter class name - put eqptRow-gen into eqptRow div

--- handistry-app/src/screens/Stockroom.tsx
+++ handistry-app/src/screens/Stockroom.tsx
@@ -9,42 +9,52 @@ import {Glassware as GlasswareModel} from '../vcl-model/Glassware';
 import {Mixture} from '../vcl-model/Mixture';
 import '../styles/style.css';
 
+/* shelfInd refers to the index of the shelf it is on. 
+   the top shelf is shelf 1, the next shelf is shelf 2, 
+   and the bottom shelf is shelf 3. */
+
+function equipmentGens2() {
+    return (
+        <div className="eqptRow" style={{"--shelfInd": 2} as React.CSSProperties}>
+            {/* list reagents here */}
+            <div className="eqptRow-gen" onClick = {() => console.log("bruh")}>
+                <Glassware
+                    data={
+                        new GlasswareModel(
+                            "erlenmeyerFlask",
+                            "../resources/img/erlenmeyerFlask.png",
+                            "../resources/img/erlenmeyerFlask-mask.png",
+                            1000,
+                            new Mixture(
+                                //@ts-ignore
+                                new Map(
+                                    [["L. water", CHEMICAL_LIST.get("H2O(l)")]]
+                                ),
+                                500
+                            ),
+                            "beaker"
+                        )
+                    }
+                />
+            </div>
+        </div>
+    );
+}
+
 function Stockroom() {
     return (
             <div className="Stockroom">
-                <div className="Stockroom-shelf">
-                    {/* list reagents here */}
-                    <div>
-                        <div className="equipmentGenerator">
-                            <Glassware
-                                data={
-                                    new GlasswareModel(
-                                        "erlenmeyerFlask",
-                                        "../resources/img/erlenmeyerFlask.png",
-                                        "../resources/img/erlenmeyerFlask-mask.png",
-                                        1000,
-                                        new Mixture(
-                                            //@ts-ignore
-                                            new Map(
-                                                [["L. water", CHEMICAL_LIST.get("H2O(l)")]]
-                                            ),
-                                            500
-                                        ),
-                                        "beaker"
-                                    )
-                                }
-                            />
-                        </div>
-                    </div>
+                {equipmentGens2()}
+                <div className="Stockroom-shelf" style={{"--shelfInd": 1} as React.CSSProperties}>
                     <div className="Stockroom-shelf-top"></div>
                 </div>
-                <div className="Stockroom-shelf">
+                <div className="Stockroom-shelf" style={{"--shelfInd": 2} as React.CSSProperties}>
                     <div className="Stockroom-shelf-top"></div>
                 </div>
-                <div className="Stockroom-shelf">
+                <div className="Stockroom-shelf" style={{"--shelfInd": 3} as React.CSSProperties}>
                     <div className="Stockroom-shelf-top"></div>
                 </div>
-                <div className="Stockroom-shelf">
+                <div className="Stockroom-shelf" style={{"--shelfInd": 4} as React.CSSProperties}>
                     <div className="Stockroom-shelf-top"></div>
                 </div>
             </div>
